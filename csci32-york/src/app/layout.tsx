f544a3962dcf54271f39d4383c2cf385317566cb
BioClonerzz <bradyork2005@gmail.com>
2024-09-09 10:10:37 -0700

    fix build error

--- csci32-york/src/app/layout.tsx
+++ csci32-york/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import '@repo/ui/styles.css'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
